b29b1bb0a779f77984b8d7b50dd0209bdf881878
AdmiralJuicy <alucard.alucard21@gmail.com>
2023-01-13 20:58:05 +0200

    added environment variables

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  authApi: 'https://montenegroauthorizationapi.azurewebsites.net/',
+  montenegroApi: 'https://montenegroapi.azurewebsites.net/',
+  tokenWhiteListedDomains: ['https://montenegroapi.azurewebsites.net/']
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  authApi: 'https://montenegroauthorizationapi.azurewebsites.net/',
+  montenegroApi: 'https://montenegroapi.azurewebsites.net/',
+  tokenWhiteListedDomains: ['https://montenegroapi.azurewebsites.net/']
+}
