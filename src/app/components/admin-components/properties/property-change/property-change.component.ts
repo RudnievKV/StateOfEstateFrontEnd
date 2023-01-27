daf71c47bbd8d3abcc86b157b8475679c8b4015e
Dondirator <147852147852l@gmail.com>
2023-01-27 16:44:52 +0200

    gfg
    ff

--- src/app/components/admin-components/properties/property-change/property-change.component.ts
+++ src/app/components/admin-components/properties/property-change/property-change.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import {FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-property-change',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./property-change.component.scss']
 })
 export class PropertyChangeComponent {
+  value = '';
+  value1 = '';
+  value2 = '';
 
+
+  toppings = new FormControl('');
+
+  toppingList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
+
+
+  toppings1 = this._formBuilder.group({
+    pepperoni: false,
+    extracheese: false,
+    mushroom: false,
+  });
+  constructor(private _formBuilder: FormBuilder) {}
+
+
+  display:any;
+  center: google.maps.LatLngLiteral = {lat:24, lng:12};
+  zoom = 4;
 }
