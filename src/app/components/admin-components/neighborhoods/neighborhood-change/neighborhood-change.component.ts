f470137e86bcaba2c15af79a03eace68042cc535
Dondirator <147852147852l@gmail.com>
2023-01-27 18:28:08 +0200

    added change-neighborhood page
    gg

--- src/app/components/admin-components/neighborhoods/neighborhood-change/neighborhood-change.component.ts
+++ src/app/components/admin-components/neighborhoods/neighborhood-change/neighborhood-change.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./neighborhood-change.component.scss']
 })
 export class NeighborhoodChangeComponent {
-
+  value = '';
 }
--- src/app/components/admin-components/partners/partner-change/partner-change.component.ts
+++ src/app/components/admin-components/partners/partner-change/partner-change.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./partner-change.component.scss']
 })
 export class PartnerChangeComponent {
+  value = '';
 
 }
