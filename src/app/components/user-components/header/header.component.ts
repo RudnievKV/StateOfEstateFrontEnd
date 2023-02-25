212ecebe17925b5af2a03f2dda57ce68ec02001c
Dondirator <75539044+Dondirator@users.noreply.github.com>
2023-02-25 18:45:09 +0200

    burger open/close
    header ts/html/scss

--- src/app/components/user-components/header/header.component.ts
+++ src/app/components/user-components/header/header.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-
+  status: boolean = false;
+  clickEvent() {
+    this.status = !this.status;
+  }
 }
